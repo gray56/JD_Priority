ba381217f7ece424138574ae061d2dd2ca28f0ff
gray56 <99997951+gray56@users.noreply.github.com>
2022-02-18 17:03:25 -0500

    JD_Priorty

---
+++ JD_Priority/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.JD_logs = true
+Config.JD_logsChannel = "priority"
+Config.JD_logsColor = "#F1F1F1"
+Config.Color = "~c~"
+
+-- Ace perm needed for staff commands
+Config.AcePerm = "jd.staff"
+
+-- Default cooldown time
+Config.CooldownTime = 15
+Config.DisplaySeconds = true
+
+-- Location for the Priorty Status Display
+Config.DisplayX = 0.165
+Config.DisplayY = 0.900
